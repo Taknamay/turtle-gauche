4383cdf219ff48ba731a3a9da5bb591b328cc13d
Jason K. MacDuffie <taknamay@gmail.com>
2015-08-29 16:56:06 -0400

    Convert everything to an R7RS library

--- canvas.scm
+++ canvas.scm
@@ -1,16 +1,16 @@
-
-(import (scheme base)
-        (tk))
-
-(begin
-  (tk-init '())
-  (tk-wm 'title "." "turtle")
-  (tk-grid (tk-canvas '.canvas '-width 600 '-height 600))
-  (define (draw-line x1 y1 x2 y2)
-    (tk-call '.canvas
-             'create
-             'line
-             (+ 300 x1)
-             (+ 300 y1)
-             (+ 300 x2)
-             (+ 300 y2))))
+(define-library (turtle canvas)
+  (import (scheme base)
+          (tk))
+  (export draw-line)
+  (begin
+    (tk-init '())
+    (tk-wm 'title "." "turtle")
+    (tk-grid (tk-canvas '.canvas '-width 600 '-height 600))
+    (define (draw-line x1 y1 x2 y2)
+      (tk-call '.canvas
+               'create
+               'line
+               (+ 300 x1)
+               (+ 300 y1)
+               (+ 300 x2)
+               (+ 300 y2)))))
--- vector.scm
+++ vector.scm
@@ -1,26 +1,27 @@
-
-(import (scheme base)
-        (scheme inexact))
-
-(define (rotate vec pvec theta-degs)
-  (define theta (degrees->radians theta-degs))
-  (add-vectors (scale-vector vec
-                             (cos theta))
-               (scale-vector pvec
-                             (sin theta))))
-
-(define (add-vectors . vs)
-  (list->vector (apply map + (map vector->list vs))))
-
-(define (scale-vector v k)
-  (vector-map (lambda (x)
-                (* x k))
-              v))
-
-(define (negate-vector v)
-  (scale-vector v -1))
-
-(define pi (* 2 (acos 0)))
-
-(define (degrees->radians degs)
-  (* degs (/ pi 180)))
+(define-library (turtle vector)
+  (import (scheme base)
+          (scheme inexact))
+  (export rotate add-vectors scale-vector negate-vector)
+  (begin
+    (define (rotate vec pvec theta-degs)
+      (define theta (degrees->radians theta-degs))
+      (add-vectors (scale-vector vec
+                                 (cos theta))
+                   (scale-vector pvec
+                                 (sin theta))))
+    
+    (define (add-vectors . vs)
+      (list->vector (apply map + (map vector->list vs))))
+    
+    (define (scale-vector v k)
+      (vector-map (lambda (x)
+                    (* x k))
+                  v))
+    
+    (define (negate-vector v)
+      (scale-vector v -1))
+    
+    (define pi (* 2 (acos 0)))
+    
+    (define (degrees->radians degs)
+      (* degs (/ pi 180)))))
