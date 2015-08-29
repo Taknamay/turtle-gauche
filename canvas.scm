6d77afbe4209f91662c637b00b0efa832d987109
Jason K. MacDuffie <taknamay@gmail.com>
2015-08-29 16:49:12 -0400

    Make the canvas larger

--- canvas.scm
+++ canvas.scm
@@ -5,6 +5,12 @@
 (begin
   (tk-init '())
   (tk-wm 'title "." "turtle")
-  (tk-grid (tk-canvas '.canvas))
+  (tk-grid (tk-canvas '.canvas '-width 600 '-height 600))
   (define (draw-line x1 y1 x2 y2)
-    (tk-call '.canvas 'create 'line x1 y1 x2 y2)))
+    (tk-call '.canvas
+             'create
+             'line
+             (+ 300 x1)
+             (+ 300 y1)
+             (+ 300 x2)
+             (+ 300 y2))))
