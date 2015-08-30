e5066ba57a482d7924a65c29c2fdcb36b1967539
Jason K. MacDuffie <taknamay@gmail.com>
2015-08-30 11:06:25 -0400

    Add spaces between procedure definitions

--- canvas.scm
+++ canvas.scm
@@ -6,6 +6,7 @@
           canvas-image-rotate)
   (begin
     (define canvas-image-rotate #f)
+
     (define (canvas-bg-color color)
       (if (memq color valid-colors)
           (tk-call '.canvas
@@ -13,17 +14,23 @@
                    '-bg
                    color)
           (error "bg-color" "Not a valid color")))
+
     (define (canvas-line-color color)
       (if (memq color valid-colors)
           (set! current-line-color color)
           (error "bg-color" "Not a valid color")))
+
     (define current-line-width 1)
+
     (define (canvas-line-width width)
       (set! current-line-width width))
+
     (define current-line-color 'white)
+
     (define valid-colors 
       '(white black red yellow green cyan blue magenta
         grey darkred darkgreen darkblue violet))
+
     (define (draw-line x1 y1 x2 y2)
       (tk-call '.canvas
                'create
@@ -36,6 +43,7 @@
                current-line-color
                '-width
                current-line-width))
+
     (tk-init '())
     (tk-wm 'title "." "turtle")
     (tk-grid (tk-canvas '.canvas '-width 600 '-height 600 '-bg 'black))))
