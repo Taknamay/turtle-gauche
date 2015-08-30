345e330e2a464de4512cde8c080eea74231c130c
Jason K. MacDuffie <taknamay@gmail.com>
2015-08-30 11:19:09 -0400

    Start drawing the turtle on the screen

--- canvas.scm
+++ canvas.scm
@@ -1,6 +1,7 @@
 
 (define-library (turtle canvas)
   (import (scheme base)
+          (scheme inexact)
           (tk))
   (export draw-line canvas-line-color canvas-bg-color canvas-line-width
           canvas-image-rotate)
@@ -44,6 +45,26 @@
                '-width
                current-line-width))
 
+    (define (redraw-turtle x y theta)
+      (define (draw-circle x y size)
+        (tk-call '.canvas
+                 'create
+                 'oval
+                 (- x (/ size 2.0))
+                 (- y (/ size 2.0))
+                 (+ x (/ size 2.0))
+                 (+ y (/ size 2.0))
+                 '-fill
+                 current-line-color))
+      ; Create the body
+      (draw-circle x y 32)
+      ; Create the head
+      (draw-circle (+ x (* 24 (cos theta)))
+                   (+ y (* 24 (sin theta)))
+                   16))
+
     (tk-init '())
     (tk-wm 'title "." "turtle")
-    (tk-grid (tk-canvas '.canvas '-width 600 '-height 600 '-bg 'black))))
+    (tk-grid (tk-canvas '.canvas '-width 600 '-height 600 '-bg 'black))
+    (redraw-turtle 300 300 0)
+))
