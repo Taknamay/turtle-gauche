c4da8af4d4a9cd2666d4e47a80aa4e335380a3e3
Jason K. MacDuffie <taknamay@gmail.com>
2015-08-29 20:16:21 -0400

    Set the default canvas and line colors

--- canvas.scm
+++ canvas.scm
@@ -5,7 +5,7 @@
   (begin
     (tk-init '())
     (tk-wm 'title "." "turtle")
-    (tk-grid (tk-canvas '.canvas '-width 600 '-height 600))
+    (tk-grid (tk-canvas '.canvas '-width 600 '-height 600 '-bg 'black))
     (define (draw-line x1 y1 x2 y2)
       (tk-call '.canvas
                'create
@@ -13,4 +13,6 @@
                (+ 300 x1)
                (+ 300 y1)
                (+ 300 x2)
-               (+ 300 y2)))))
+               (+ 300 y2)
+               '-fill
+               'white))))
