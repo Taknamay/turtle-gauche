62ca3990d1a8b492307ebf362eb1e093482911db
Jason K. MacDuffie <taknamay@gmail.com>
2015-08-29 20:28:13 -0400

    Get started on custom line and background colors

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Gauche-tk Turtle
 ================
-This is a turtle program that runs in Gauche-tk. However, the code is modular enough that it should work with any "draw-line" function.
+This is a turtle program that runs in Gauche-tk. However, the code is modular enough that it should work with any "draw-line" procedures.
+
+Also, "canavs-line-color" and "canvas-bg-color" are nice, but not necessary procedures to include.
--- canvas.scm
+++ canvas.scm
@@ -1,11 +1,10 @@
 (define-library (turtle canvas)
   (import (scheme base)
           (tk))
-  (export draw-line)
+  (export draw-line canvas-line-color canvas-bg-color)
   (begin
-    (tk-init '())
-    (tk-wm 'title "." "turtle")
-    (tk-grid (tk-canvas '.canvas '-width 600 '-height 600 '-bg 'black))
+    (define canvas-line-color #f)
+    (define canvas-bg-color #f)
     (define (draw-line x1 y1 x2 y2)
       (tk-call '.canvas
                'create
@@ -15,4 +14,7 @@
                (+ 300 x2)
                (+ 300 y2)
                '-fill
-               'white))))
+               'white))
+    (tk-init '())
+    (tk-wm 'title "." "turtle")
+    (tk-grid (tk-canvas '.canvas '-width 600 '-height 600 '-bg 'black))))
