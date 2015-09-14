5b74d1d6dab1377cab794e846b46b110f50b5cea
Jason K. MacDuffie <taknamay@gmail.com>
2015-09-14 09:39:59 -0400

    Make the turtle stay on the line

--- canvas.scm
+++ canvas.scm
@@ -83,7 +83,8 @@
                '-width
                current-line-width))
 
-    (define (redraw-turtle x y theta-deg turtle-size)
+    (define (redraw-turtle x y-neg theta-deg turtle-size)
+      (define y (- y-neg))
       (define theta (* theta-deg (/ 3.141592653589792 180)))
       (define (draw-circle x y size tags)
         (tk-call '.canvas
